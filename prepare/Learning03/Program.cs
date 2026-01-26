80c07877c573a0c291f2d7b4bcf4bbb64787c655
monster828 <aldenrshurtz@gmail.com>
2026-01-26 09:41:33 -0600

    new file:   prepare/Learning03/Fraction.cs 	modified:   prepare/Learning03/Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction = new();
+        Random random = new();
+
+        for (int i = 0; i < 20; i++)
+        {
+            int top = random.Next();
+            int bottom = random.Next();
+            fraction.SetTop(top);
+            fraction.SetBottom(bottom);
+
+            Console.WriteLine($"Fraction {i}: string: {fraction.GetFractionString()}, Number: {fraction.GetDecimalValue()}");
+        }
     }
 }
